4b79e085561a3524c65304cc5cf1c5a2d7da604f
mehdibada1-gif <mehdibada1@gmail.com>
2025-12-07 12:57:13 +0100

    genkit

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,12 @@
+'use server';
+import { config } from 'dotenv';
+config();
+
 import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/google-genai'; 
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({ apiKey: process.env.GEMINI_API_KEY })],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
 });
